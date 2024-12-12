a3daac94f9c7c4b87e23a36f975ed3eae056e88b
helloimalemur <jkoontsiii@gmail.com>
2024-12-12 18:38:30 -0500

    clippy

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() {
 
     get_repo_from_config(&config_dir)
         .iter_mut()
-        .for_each(|mut repo| {
+        .for_each(|repo| {
             prepare(repo);
             state.add_repo(repo.clone().name, repo.to_owned())
         });
