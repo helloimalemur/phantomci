121a8b79ee20e3cbe11eee23d76f127b9a158d85
helloimalemur <james@koonts.net>
2026-01-08 22:23:15 -0500

    custom webhook

--- src/webhook/mod.rs
+++ src/webhook/mod.rs
@@ -4,5 +4,3 @@ pub mod custom_webhook;
 pub mod webhook_config;
 
 pub use crate::webhook::discord::{Webhook, WebhookConfig, WebhookType};
-pub use crate::webhook::slack::{send_slack};
-pub use crate::webhook::custom_webhook::{send_custom};
